2253e31c0b43953040a968852dd120147ba5169f
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-09-13 08:21:18 +0200

    Remove the-fountainai dependency from Package.swift
    Eliminates the external 'the-fountainai' package and its related target dependencies from the TutorCLI project, simplifying the build configuration.

--- tools/tutor-cli/Package.swift
+++ tools/tutor-cli/Package.swift
@@ -7,15 +7,11 @@ let package = Package(
     products: [
         .executable(name: "tutor", targets: ["TutorCLI"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/Fountain-Coach/the-fountainai.git", branch: "main")
-    ],
+    dependencies: [],
     targets: [
         .executableTarget(
             name: "TutorCLI",
-            dependencies: [
-                .product(name: "SSEOverMIDI", package: "the-fountainai")
-            ],
+            dependencies: [],
             resources: [
                 .process("OpenAPI")
             ],
